494b099e55af7f70a17113855bb68155a53a0988
venkata.elluru <venkata.elluru@ril.com>
2022-11-03 20:43:08 +0530

    updated code on encrypt message code

--- src/test/java/practice/coding/leetcode/MessageMasking.java
+++ src/test/java/practice/coding/leetcode/MessageMasking.java
@@ -1,19 +1,63 @@
 package practice.coding.leetcode;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Hashtable;
+import java.util.*;
 import java.util.stream.Stream;
 
 /**
  * https://leetcode.com/problems/decode-the-message/
  */
-public class MessageMasking {
+public class MessageMasking extends SecretSociety{
 
     @Test
     public void testMethod(){
-        System.out.println(MessageMasking.decodeMessage("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv"));;
+        System.out.println(MessageMasking.decodeMessage_upgrade("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv"));;
+    }
+
+    @Test
+    public void testMethod2(){
+        Assert.assertEquals(MessageMasking.decodeMessage("szwr wr j rfmofs ifrrjpf"), "this is a secret message");
+        Assert.assertEquals(MessageMasking.decodeMessage("zfhhg jvwg"), "hello ajio");
+    }
+    static char[] alphabets = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+    public static String decodeMessage(String message) {
+        String decryptedMessage = "";
+
+        char c;
+        // for (char c: message.toCharArray()) { // O(n^2) forloop iterates for n times and toCharArray() is O(n)
+        // decryptedMessage += Arrays.asList(maskKey).indexOf(c); // O(m)
+        for(int i = 0 ; i < message.length() ; i++){
+            decryptedMessage += ((c = message.charAt(i)) != ' ') ? alphabets[findIndex(c, 0, maskKey)] : c;
+        }
+        return decryptedMessage;
+    }
+
+    // logically findIndex time complexity is O(1)
+    private static int findIndex(char expectedChar, int index, char[] maskKey) {
+        return (maskKey[index] == expectedChar) ? index : findIndex(expectedChar, index + 1, maskKey);
+    }
+
+    public static String decodeMessage_upgrade(String key, String message) {
+        char[] alphabets = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        char[] keyMapping = new char[26];
+        int alphaIndex = 0;
+        key = key.replaceAll(" ", "");
+        char c;
+        do {
+            c = key.charAt(0);
+            if (c != ' ' && keyMapping[c - 'a'] == 0){
+                keyMapping[c - 'a'] = alphabets[alphaIndex++];
+            }
+            key = key.replaceAll(c+"", "");
+        } while(key.length() > 0);
+
+        for (char cc : message.toCharArray()){
+            key += ( cc != ' ' ? keyMapping[cc - 'a'] : cc );
+        }
+        return key;
     }
 
     /**
@@ -82,4 +126,8 @@ public class MessageMasking {
         }
         return result;
     }
+}
+
+class SecretSociety{
+    protected static char[] maskKey = {'j','q','m','u','f','e','p','z','w','v','k','h','i','l','g','d','b','o','r','s','t','n','a','x','y','c'};
 }
